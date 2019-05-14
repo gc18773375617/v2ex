d952d27c47e22356de89917b97542253055bb6d6
gc18773375617 <654992676@qq.com>
2019-05-14 17:14:25 +0800

    列表样式

--- src/dom/list.js
+++ src/dom/list.js
@@ -1,17 +1,59 @@
 import React,{Component} from 'react'
 import axios from 'axios'
-
+import '../css/list.scss'
 class List extends Component {
-	componentDidMount=()=>{
-		axios.get("/api/topics/hot.json")
-		.then(res=>{
-			
-		});
-		console.log(this)
+	constructor(){
+		super();
+		this.state={
+			list:[],
+			ty:""
+		}
 	}
+	componentDidMount=()=>{
+		this.getData()
+	}
+	getData=()=>{
+		var type = this.props.match.params.type;
+		axios.get("/api/topics/show.json?node_name="+type)
+		.then(res=>{
+			console.log(res)
+			const data = res.data
+			this.setState({
+				list:data.map((i,vo)=>{
+					return(
+						<div key={vo} className="list_item">
+							<ul>
+								<li>
+									<img src={i.member.avatar_mini} alt=""/>
+								</li>
+								<li className="item_center">
+									<h1><i>{i.node.title}</i> • <span>{i.member.username}</span> </h1>
+									<p>{i.title}</p>
+									<h2>11 分钟前  •  最后回复 <span>{i.last_reply_by}</span></h2>
+								</li>
+								<li>
+									<i><span>{i.replies}</span></i>
+								</li>
+							</ul>
+						</div>
+					)
+				}
+			),
+			ty:type
+			})
+		})
+	}
+	componentDidUpdate() {
+		console.log(this.state.ty !== this.props.match.params.type)
+		if(this.state.ty !== this.props.match.params.type){
+			this.getData();
+		}
+	 }
 	render(){
 		return(
-		<h1>this is List</h1>
+		<div>
+			{this.state.list}
+		</div>
 		)
 	}
 	
--- src/dom/nav.js
+++ src/dom/nav.js
@@ -2,20 +2,26 @@ import React,{ Component } from 'react';
 import {Link,withRouter} from 'react-router-dom';
 import '../css/nav.scss';
 import img12 from '../img/v2ex.png';
-import Router from '../router'
-import axios from 'axios'
+import Router from '../router';
 class appNav extends Component{
     constructor(){
         super();
         this.state = {
-            query:"creative"
+            query:""
         }
-        console.log(this)
     }
     componentDidMount(){
-        axios.get("/api/nodes/all.json")
-        .then((res)=>{
-            console.log(res)
+        this.getQuery()
+    }
+    componentDidUpdate=()=>{
+        if(this.state.query !== this.props.location.pathname){
+            this.getQuery()
+        }
+    }
+    getQuery=()=>{
+        const que = this.props.location.pathname
+        this.setState({
+            query:que
         })
     }
     render(){
@@ -27,21 +33,20 @@ class appNav extends Component{
                 <div className="box_border">
                     <div className="box">
                         <div className="link">
-                            <Link className={this.state.query === "tech"?"link_selected":""} to="/list/tech">技术</Link>
-                            <Link className={this.state.query === "creative"?"link_selected":""} to="/list/creative">创意</Link>
-                            <Link className={this.state.query === "play"?"link_selected":""} to="/list">好玩</Link>
-                            <Link className={this.state.query === "apple"?"link_selected":""} to="/list">Apple</Link>
-                            <Link className={this.state.query === "jobs"?"link_selected":""} to="/list">酷工作</Link>
-                            <Link className={this.state.query === "deals"?"link_selected":""} to="/list">交易</Link>
-                            <Link className={this.state.query === "city"?"link_selected":""} to="/list">城市</Link>
-                            <Link className={this.state.query === "qna"?"link_selected":""} to="/list">问与答</Link>
-                            <Link className={this.state.query === "hot"?"link_selected":""} to="/list">最热</Link>
-                            <Link className={this.state.query === "all"?"link_selected":""} to="/list">全部</Link>
-                            <Link className={this.state.query === "r2"?"link_selected":""} to="/list">R2</Link>
+                            <Link className={this.state.query === "/list/tech"?"link_selected":""} to="/list/tech">技术</Link>
+                            <Link className={this.state.query === "/list/creative"?"link_selected":""} to="/list/creative">创意</Link>
+                            <Link className={this.state.query === "/list/play"?"link_selected":""} to="/list/play">好玩</Link>
+                            <Link className={this.state.query === "/list/apple"?"link_selected":""} to="/list/apple">Apple</Link>
+                            <Link className={this.state.query === "/list/jobs"?"link_selected":""} to="/list/jobs">酷工作</Link>
+                            <Link className={this.state.query === "/list/deals"?"link_selected":""} to="/list/deals">交易</Link>
+                            <Link className={this.state.query === "/list/city"?"link_selected":""} to="/list/city">城市</Link>
+                            <Link className={this.state.query === "/list/qna"?"link_selected":""} to="/list/qna">问与答</Link>
+                            <Link className={this.state.query === "/list/hot"?"link_selected":""} to="/list/hot">最热</Link>
+                            <Link className={this.state.query === "/list/all"?"link_selected":""} to="/list/all">全部</Link>
+                            <Link className={this.state.query === "/list/r2"?"link_selected":""} to="/list/r2">R2</Link>
                         </div>
                         <Router/>
                     </div>
-                    
                 </div>
             </div>
         )
